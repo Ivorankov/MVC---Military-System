d2df36b051692047cd7f43cfde715014231f0436
Ivcho <ivo_rankov@mail.bg>
2016-02-10 00:17:44 +0200

    Added Repository + Data context
    Todo Data seed

---
+++ MilitarySystem/MilitarySystem.Data/Contracts/IMilitarySystemContext.cs
@@ -0,0 +1,36 @@
+﻿namespace MilitarySystem.Data.Contracts
+{
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    using MilitartySystem.Models;
+
+    public interface IMilitarySystemContext
+    {
+        IDbSet<User> Users { get; set; }
+
+        IDbSet<Platoon> Platoons { get; set; }
+
+        IDbSet<Squad> Squads { get; set; }
+
+        IDbSet<Manufacturer> Manufacturers { get; set; }
+
+        IDbSet<Message> Messages { get; set; }
+
+        IDbSet<Mission> Missions { get; set; }
+
+        IDbSet<Gear> Gear { get; set; }
+
+        IDbSet<Weapon> Weapons { get; set; }
+
+        IDbSet<Vehicle> Vehicles { get; set; }
+
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        DbEntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        void Dispose();
+
+        int SaveChanges();
+    }
+}
