d72c8d5b98f4c8a31314502b17a22fd5d70862c5
Ivcho <ivo_rankov@mail.bg>
2016-02-13 22:52:17 +0200

    Basic separation

---
+++ MilitarySystem/MilitarySystem.Web/Areas/Troops/TroopsAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MilitarySystem.Web.Areas.Troops
+{
+    public class TroopsAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Troops";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Troops_default",
+                "Troops/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
