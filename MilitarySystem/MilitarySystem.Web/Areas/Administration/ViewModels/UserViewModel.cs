5a305d14d785578c0b0dc6979366c52211d21c0c
Ivcho <ivo_rankov@mail.bg>
2016-02-15 15:57:43 +0200

    Some emptry view models

---
+++ MilitarySystem/MilitarySystem.Web/Areas/Administration/ViewModels/UserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MilitarySystem.Web.Areas.Administration.ViewModels
+{
+    public class UserViewModel
+    {
+    }
+}
