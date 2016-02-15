b7cc29c854a7783cac9f2f6499cc29ab6ffd8e0c
Ivcho <ivo_rankov@mail.bg>
2016-02-15 13:40:11 +0200

    More views + better structure

---
+++ MilitarySystem/MilitarySystem.Web/Areas/Administration/Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MilitarySystem.Web.Areas.Administration.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Administration/Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
